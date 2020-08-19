877f65bd26c6929299f038f1c8c9759d5610402f
davjvo <davjvo.dev@gmail.com>
2020-08-19 19:52:44 -0400

    pase a react

---
+++ precache-manifest.6d44aaf54f746015d6467373d5b07e96.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cc5423f2d7e7a2817487102a97be09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8570554fda947e7d744d",
+    "url": "/static/css/main.5028a408.chunk.css"
+  },
+  {
+    "revision": "8c38ece9afca214d7fc7",
+    "url": "/static/js/2.ba7e178e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ba7e178e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8570554fda947e7d744d",
+    "url": "/static/js/main.347f4ab7.chunk.js"
+  },
+  {
+    "revision": "87c14c9b2478cfb3522d",
+    "url": "/static/js/runtime-main.e8b920fb.js"
+  },
+  {
+    "revision": "49c05d023529469241167ffd332c51e6",
+    "url": "/static/media/amsipro.49c05d02.woff2"
+  },
+  {
+    "revision": "6d8d5845358a21fe3b3be90abc3ff629",
+    "url": "/static/media/amsipro.6d8d5845.woff"
+  },
+  {
+    "revision": "47c4b68ddf179d2668de1284903a7568",
+    "url": "/static/media/brandontextbold.47c4b68d.woff2"
+  },
+  {
+    "revision": "53f27b740674c3669047dc59be51dfe7",
+    "url": "/static/media/brandontextbold.53f27b74.woff"
+  },
+  {
+    "revision": "115ac263370f0646087a4177ba28b325",
+    "url": "/static/media/indice_acreditacion1.115ac263.png"
+  },
+  {
+    "revision": "d50f2632fa73002c31fc4d49744f8a92",
+    "url": "/static/media/indice_acreditacion2.d50f2632.svg"
+  },
+  {
+    "revision": "77c93270fbacc47b9073ea89ef318698",
+    "url": "/static/media/indice_banner.77c93270.png"
+  },
+  {
+    "revision": "78b50914226a852ef81452272cd020ed",
+    "url": "/static/media/logo_anime_island.78b50914.png"
+  },
+  {
+    "revision": "ca65963f04759f53a7e21809a6e4d966",
+    "url": "/static/media/logo_sketchdom.ca65963f.png"
+  }
+]);
