2293d85e5926484881b6ee25cd8073127e905894
davjvo <davjvo.dev@gmail.com>
2021-11-18 23:30:47 -0400

    2021 v1

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function resize() {
-    if (window.outerWidth <= 992 || window.outerHeight < 1050) {
-        window.location.replace(window.location.origin + '/mw' + window.location.pathname);
-    }
-}
-
-window.onload = resize;
-
-window.onresize = resize;
